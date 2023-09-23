34866cf0f6d0e3b193495c34848cc41ee5bb5c7c
stephen-fraser <steve.james.fraser@gmail.com>
2023-09-23 16:07:25 -0700

    first commit index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (toN === fromN){
+    return toN;
+  }
+  return toN + sum(fromN, toN - 1);
 }
 
 module.exports = sum;
